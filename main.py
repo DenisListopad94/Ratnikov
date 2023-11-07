761f3d14438bfd39f8ccf8b5fca1d3287d77486e
Ratnikov <iamratnikov@yandex.by>
2023-11-07 13:18:56 +0300

    correct task1

--- main.py
+++ main.py
@@ -24,24 +24,30 @@ weeks = days // 7
 print(("№6"), weeks, "полных недель")
 
 #7
-side_1 = 150
-side_2 = 65
+side_1 = int(input("Введите сторону a: "))
+side_2 = int(input("Введите сторону b: "))
 squre = 30
-squres = (150 // 30) * (65 // 30)
+squres = (side_1 // 30) * (side_2 // 30)
 print("№7", "Можно отрезать", squres, "квадратов")
 
 #8
-seconds = 4000
+seconds = int(input("Введите количество секунд: "))
 hours = seconds // 3600
-minutes = 400 // 60
-seconds = 40
+seconds %= 3600
+minutes = seconds // 60
+seconds %= 60
 print(("№8"), hours, "час", minutes, "минут", seconds, "секунд")
 
+
 #9
-cash = 361
+cash = int(input("Cash: "))
 hundred = cash // 100
-fifty = 61 // 50
-ten = 11 // 10
-one = 1
+cash %= 100
+fifty = cash // 50
+cash %= 50
+ten = cash // 10
+cash %= 10
+one = cash // 1
+cash %= 1
 print(("№9"), hundred,"купюры по 100 рублей,", fifty, "купюра по 50 рублей,", ten, "купюра по 10 рублей,", one, "купюра по 1 рублю.")
 
